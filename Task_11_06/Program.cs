975a90e64c34e3e1a15a0aebee9e177d2378e302
Zlata_Evg <zlataevgrafova546@gmail.com>
2025-03-19 22:13:03 +0500

    Выполнена 6 задача 11 урока

---
+++ Task_11_06/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task_11_06
+{
+    internal class Program
+    {
+        /*Передача массива по значению: Напишите метод, который принимает массив целых чисел и
+         * изменяет его элементы, увеличивая каждый на 1. Проверьте, изменился ли оригинальный
+         * массив вне метода.
+         */
+        static void Main(string[] args)
+        {
+            // создаем и заполняем массив случайными целыми числами
+            Random rnd = new Random();
+            int[] Array = new int[10];
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Array[i] = rnd.Next(1, 100);
+            }
+            //выводим изначальный массив и после изменения
+            Console.WriteLine("Изначальный массив:");
+            foreach (int number in Array)
+            {
+                Console.WriteLine(number);
+            }
+
+            Array2(Array);
+
+            Console.WriteLine("Массив после изменения его элементов:");
+            foreach (int number in Array)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        /// <summary>
+        /// Увеличивает каждый элемент массива на 1.
+        /// </summary>
+        /// <param name="array">Массив целых чисел, элементы которого будут увеличены.</param>
+        static void Array2(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] += 1;
+            }
+        }
+    }
+}
