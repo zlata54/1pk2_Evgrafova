f37586f99387f5b2d90d5a5261742f1646d731ab
Zlata_Evg <zlataevgrafova546@gmail.com>
2025-03-19 20:47:49 +0500

    Выполнена 1 задача 11 урока

--- Task_11_01/Program.cs
+++ Task_11_01/Program.cs
@@ -7,47 +7,29 @@
          */
         static void Main(string[] args)
         {
-            // Запрашиваем у пользователя два числа и выводим их 
-            Console.Write("Введите первое целое число: ");
+            //запрашиваем два целых числа и выводим их до и после вызова метода
+            Console.Write("Введите первое число: ");
             int x = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Введите второе целое число: ");
+            Console.Write("Введите второе число: ");
             int y = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Порядок чисел до изменения: x = {x}, y = {y}");
+            Console.WriteLine($"Значения переменных перед вызовом метода: x = {x}, y = {y}");
 
-            //сохраняем изначальные значения для последующей проверки
-            int originalA = x;
-            int originalB = y;
-            
-            Swap(ref x, ref y);
-            //Проверяем, изменились ли значения переменных вне метода
-            Console.WriteLine($"Порядок чисел после изменения: x = {x}, y = {y}");
-            if (originalA != x || originalB != y)
-            {
-                Console.WriteLine("Значения переменных изменились.");
-            }
-            else
-            {
-                Console.WriteLine("Значения переменных остались прежними.");
-            }
+            Swap(x, y);
+
+            Console.WriteLine($"После вызова метода: x = {x}, y = {y}");
         }
-        
 
-        /// <summary>
-        /// Обменивает местами два целых числа.
-        /// </summary>
-        /// <param name="x">Первое число.</param>
-        /// <param name="y">Второе число.</param>
-        static void Swap(ref int x, ref int y)
-            {
-                int a = x;
-                x = y;
-                y = a;
-            }
-
+        static void Swap(int x, int y)
+        {
+            int a = x;
+            x = y;      
+            y = a;    
+            Console.WriteLine($"Внутри метода: x = {x}, y = {y}");
         }
 
     }
+}
     
 
