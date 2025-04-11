36334495b545de1b422d86a2c0e7680d95700821
Zlata_Evg <zlataevgrafova546@gmail.com>
2025-04-12 03:47:29 +0500

    выполнена 6 задача 21 урока

---
+++ Task_21_06/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Task_21_06
+{
+    internal class Program
+    {
+        /*Написать метод, который удаляет повторяющиеся элементы из списка, сохраняя порядок
+         */
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int> { 5, 94, 2, 3, 1, 492, 5, 5, 6, 1, 8, 94, 3, 23, 65, 5 };
+
+            Console.WriteLine("Исходный список: ");
+            PrintList(myList);
+
+            List<int> resultList = RemoveDuplicates(myList);
+
+            Console.WriteLine("Список без дубликатов: ");
+            PrintList(resultList);
+        }
+        /// <summary>
+        /// Удаляет дубликаты из заданного списка целых чисел.
+        /// </summary>
+        /// <param name="list">Список целых чисел, из которого необходимо удалить дубликаты.</param>
+        /// <returns>Новый список целых чисел без дубликатов.</returns>
+        static List<int> RemoveDuplicates(List<int> list)
+        {
+            List<int> uniqueList = new List<int>();
+
+            foreach (int item in list)
+            {
+                // Проверяем, есть ли элемент уже в уникальном списке
+                if (!uniqueList.Contains(item))
+                {
+                    uniqueList.Add(item);
+                }
+            }
+            return uniqueList;
+        }
+        /// <summary>
+        /// Выводит элементы заданного списка целых чисел в консоль.
+        /// </summary>
+        /// <param name="list">Список целых чисел для вывода.</param>
+        static void PrintList(List<int> list)
+        {
+            foreach (int number in list)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
